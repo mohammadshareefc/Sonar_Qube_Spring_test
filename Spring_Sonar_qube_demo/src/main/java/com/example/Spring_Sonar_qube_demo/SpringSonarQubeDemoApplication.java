590cf4e611ac728c0667c5ff6be3acef78f917e9
Shareef <163128705+mohammadshareefc@users.noreply.github.com>
2024-09-10 16:54:15 +0530

    Sonar Test

---
+++ Spring_Sonar_qube_demo/src/main/java/com/example/Spring_Sonar_qube_demo/SpringSonarQubeDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.Spring_Sonar_qube_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Main class to start the Spring SonarQube Demo application.
+ */
+@SpringBootApplication
+public class SpringSonarQubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSonarQubeDemoApplication.class, args);
+	}
+
+}
